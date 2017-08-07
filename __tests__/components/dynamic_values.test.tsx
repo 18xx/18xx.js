2a02223814425e992b739b9eebce0b8db9934cea
Gregor MacDougall <gmacdougall0+18xx-github@gmail.com>
2017-08-06 22:24:27 -0400

    Fix Dynamic Value Test

--- __tests__/components/dynamic_values.test.tsx
+++ __tests__/components/dynamic_values.test.tsx
@@ -1,18 +1,23 @@
-import DynamicValues from '../../src/components/dynamic_values';
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import DynamicValues, {
+  DynamicValueSet
+} from '../../src/components/dynamic_values';
 
 import Hexagon from '../../src/hexagon';
 
 describe('DynamicValues', () => {
   const hexagon: Hexagon = new Hexagon('east-west');
 
-  describe('#toString', () => {
+  describe('#render', () => {
     describe('when fixedHeight is not set', () => {
       it('returns svg for the values', () => {
-        const dv: DynamicValues = new DynamicValues({
-          hexagon,
-          values: { yellow: 20, green: 40 }
-        });
-        expect(dv.toString()).toMatchSnapshot();
+        const values: DynamicValueSet = { yellow: 20, green: 40 };
+        const subject: React.ReactElement<DynamicValues> = (
+          <DynamicValues hexagon={hexagon} values={values} />
+        );
+        expect(renderer.create(subject)).toMatchSnapshot();
       });
     });
 
