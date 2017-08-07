13399451f3dd1e864efc5d3c9024e8e9ee8a2b0e
Gregor MacDougall <gmacdougall0+18xx-github@gmail.com>
2017-08-07 07:32:54 -0400

    Distinct City Coverage
    This adds test coverage for distinct cities

--- __tests__/components/distinct_city.test.tsx
+++ __tests__/components/distinct_city.test.tsx
@@ -1,9 +1,54 @@
-import CityCircle from '../../src/components/city_circle';
-import DistinctCity from '../../src/components/distinct_city';
-import Line from '../../src/components/line';
+import { List } from 'immutable';
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
 
+import { mapDefinition } from '../support/map_definition';
+
+import Hexagon from '../../src/hexagon';
 import Point from '../../src/point';
 
+import { CityProps } from '../../src/components/city';
+import DistinctCity from '../../src/components/distinct_city';
+
 describe('DistinctCity', () => {
-  it('is pending');
+  const defaultProps: CityProps =  {
+    hex: 'a1',
+    hexagon: new Hexagon('north-south'),
+    homeTokens: List(),
+    mapDef: mapDefinition,
+    num: 1,
+    onRightClickCity: jest.fn(),
+    onRightClickToken: jest.fn(),
+    points: List([new Point(30, 40)]),
+    spotLocations: [1, 2],
+    tokenState: List(),
+  };
+
+  it('renders correctly', () => {
+    const subject: React.ReactElement<DistinctCity> = (
+      <DistinctCity {...defaultProps} />
+    );
+
+    expect(renderer.create(subject)).toMatchSnapshot();
+  });
+
+  describe('when no spots are specified', () => {
+    it('raises an error', () => {
+      const subject: React.ReactElement<DistinctCity> = (
+        <DistinctCity {...defaultProps} spotLocations={undefined} />
+      );
+      expect(() => renderer.create(subject)).toThrow(
+        'DistinctCity requires spotLocations'
+      );
+    });
+  });
+
+  describe('when there are more than two spots', () => {
+    it('uses a smaller radius', () => {
+      const subject: React.ReactElement<DistinctCity> = (
+        <DistinctCity {...defaultProps} spotLocations={[1, 2, 3, 5]} />
+      );
+      expect(renderer.create(subject)).toMatchSnapshot();
+    });
+  });
 });
