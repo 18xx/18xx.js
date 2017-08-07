74af75f47029fc7e2e1939cb282126be994632dd
Gregor MacDougall <gmacdougall0+18xx-github@gmail.com>
2017-08-06 23:10:21 -0400

    Extract map definition to support
    This allows map definitions to be used in multiple tests.

--- __tests__/map_builder.test.tsx
+++ __tests__/map_builder.test.tsx
@@ -8,82 +8,18 @@ import TileSet from '../src/tile_set';
 import Game from '../src/components/game';
 import Tile from '../src/components/tile';
 
-const json: any = {
-  companies: {
-    'B&O': {
-      name: 'Baltimore and Ohio Railroad',
-      primaryColor: '#070A3F',
-      secondaryColor: 'white',
-      shorthand: 'B&O',
-      textColor: 'black',
-    }
-  },
-  costLocation: {
-    b4: { x: 5, y: 35 }
-  },
-  dynamicValues: {
-    a1: {
-      values: {
-        // FIXME: New Value System
-        brown: 50,
-        gray: 60,
-        green: 30,
-        yellow: 20,
-      }
-    }
-  },
-  hexes: {
-    a: [1, 3],
-    b: [2, 4, 6, 8]
-  },
-  names: {
-    a1: 'One Town',
-    a3: 'Threesville'
-  },
-  offBoards: {
-    a1: {
-      exits: [0, 5]
-    }
-  },
-  preplacedTile: {
-    b8: {
-      color: 'gray',
-      label: 'Preplaced',
-      spots: 1,
-      trackToCenter: [3],
-      type: 'City'
-    }
-  },
-  tileCostTypes: {
-    mountain: {
-      amount: 120,
-      color: '#f86',
-      shape: 'triangle'
-    },
-    river: {
-      amount: 40,
-      color: '#aaf',
-      shape: 'square'
-    }
-  },
-  tileCosts: {
-    b2: 'river',
-    b4: 'mountain'
-  },
-  tileManifest: {
-  },
-};
+import { mapDefinition } from './support/map_definition';
 
 const game: Game = new Game({
   gameName: '18xx',
-  mapDef: json,
+  mapDef: mapDefinition,
 });
 
 describe('MapBuilder', () => {
   describe('#build()', () => {
     it('returns an svg of the map', () => {
-      const tileSet: TileSet = new TileSet(List([]), json, Map());
-      const builder: MapBuilder = new MapBuilder(game, json, tileSet);
+      const tileSet: TileSet = new TileSet(List([]), mapDefinition, Map());
+      const builder: MapBuilder = new MapBuilder(game, mapDefinition, tileSet);
       const tileState: Map<string, string> = (
         Map() as Map<string, string>
       );
---
+++ __tests__/support/map_definition.ts
@@ -0,0 +1,68 @@
+import { MapDefinition } from '../../src/map_builder';
+
+export const mapDefinition: MapDefinition = {
+  companies: {
+    'B&O': {
+      name: 'Baltimore and Ohio Railroad',
+      primaryColor: '#070A3F',
+      secondaryColor: 'white',
+      shorthand: 'B&O',
+      textColor: 'black',
+    }
+  },
+  costLocation: {
+    b4: { x: 5, y: 35 }
+  },
+  dynamicValues: {
+    a1: {
+      values: {
+        // FIXME: New Value System
+        brown: 50,
+        gray: 60,
+        green: 30,
+        yellow: 20,
+      }
+    }
+  },
+  hexes: {
+    a: [1, 3],
+    b: [2, 4, 6, 8]
+  },
+  names: {
+    a1: 'One Town',
+    a3: 'Threesville'
+  },
+  offBoards: {
+    a1: {
+      exits: [0, 5]
+    }
+  },
+  orientation: 'east-west',
+  preplacedTile: {
+    b8: {
+      color: 'gray',
+      label: 'Preplaced',
+      spots: 1,
+      trackToCenter: [3],
+      type: 'City'
+    }
+  },
+  tileCostTypes: {
+    mountain: {
+      amount: 120,
+      color: '#f86',
+      shape: 'triangle'
+    },
+    river: {
+      amount: 40,
+      color: '#aaf',
+      shape: 'square'
+    }
+  },
+  tileCosts: {
+    b2: 'river',
+    b4: 'mountain'
+  },
+  tileManifest: {
+  },
+};
