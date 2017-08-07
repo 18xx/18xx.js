8e810296374a2b8ff25a0605dadda4ade80a16f9
Gregor MacDougall <gmacdougall0+18xx-github@gmail.com>
2017-08-06 21:43:23 -0400

    Remove unreachable condition
    props.amount is required so it won't be empty

--- src/components/value.tsx
+++ src/components/value.tsx
@@ -15,31 +15,28 @@ export interface ValueProps {
 }
 
 class Value extends React.Component<ValueProps, {}> implements TileElement {
-  public render(): ReactElement<Value> | null {
-    if (this.props.amount) {
-      return (
-        <g key='city-amount'>
-          <circle
-            key='city-amount-circle'
-            cx={this.position.x}
-            cy={this.position.y}
-            r={RADIUS}
-            fill='white'
-            stroke='#000'
-            strokeWidth='2'
-          />
-          <text
-            key='city-amount-text'
-            x={this.position.x}
-            y={this.position.y + 4}
-            textAnchor='middle'
-            fill='black'
-            fontSize='11'
-          >{this.props.amount}</text>
-        </g>
-      );
-    }
-    return null;
+  public render(): ReactElement<Value> {
+    return (
+      <g key='city-amount'>
+        <circle
+          key='city-amount-circle'
+          cx={this.position.x}
+          cy={this.position.y}
+          r={RADIUS}
+          fill='white'
+          stroke='#000'
+          strokeWidth='2'
+        />
+        <text
+          key='city-amount-text'
+          x={this.position.x}
+          y={this.position.y + 4}
+          textAnchor='middle'
+          fill='black'
+          fontSize='11'
+        >{this.props.amount}</text>
+      </g>
+    );
   }
 
   private get position(): Point {
