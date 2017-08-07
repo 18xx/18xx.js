aedf1524d8519b05eac7a2a6fd5aa0404c679b4d
Gregor MacDougall <gmacdougall0+18xx-github@gmail.com>
2017-08-07 07:43:50 -0400

    Add City Coverage

--- __tests__/components/city.test.tsx
+++ __tests__/components/city.test.tsx
@@ -1,9 +1,83 @@
-import City from '../../src/components/city';
-import CityCircle from '../../src/components/city_circle';
-import Town from '../../src/components/town';
+import { List } from 'immutable';
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
 
+import Hexagon from '../../src/hexagon';
 import Point from '../../src/point';
 
+import City, { CityProps } from '../../src/components/city';
+
 describe('City', () => {
-  it('is pending');
+  const defaultProps: CityProps =  {
+    hex: 'a1',
+    hexagon: new Hexagon('east-west'),
+    homeTokens: List(),
+    mapDef: { companies: [], tileManifest: []},
+    num: 1,
+    onRightClickCity: jest.fn(),
+    onRightClickToken: jest.fn(),
+    tokenState: List(),
+  };
+
+  describe('when there is one city', () => {
+    it('renders an svg for the city', () => {
+      const subject: React.ReactElement<City> = (
+        <City {...defaultProps} />
+      );
+
+      expect(renderer.create(subject)).toMatchSnapshot();
+    });
+  });
+
+  describe('when there are two cities', () => {
+    it('renders an svg for the cities', () => {
+      const subject: React.ReactElement<City> = (
+        <City {...defaultProps} num={2} />
+      );
+
+      expect(renderer.create(subject)).toMatchSnapshot();
+    });
+  });
+
+  describe('when there are three cities', () => {
+    it('renders an svg for the cities', () => {
+      const subject: React.ReactElement<City> = (
+        <City {...defaultProps} num={3} />
+      );
+
+      expect(renderer.create(subject)).toMatchSnapshot();
+    });
+  });
+
+  describe('when there are four cities', () => {
+    it('renders an svg for the cities', () => {
+      const subject: React.ReactElement<City> = (
+        <City {...defaultProps} num={4} />
+      );
+
+      expect(renderer.create(subject)).toMatchSnapshot();
+    });
+  });
+
+  describe('when there are five cities', () => {
+    it('throws an error', () => {
+      const subject: React.ReactElement<City> = (
+        <City {...defaultProps} num={5} />
+      );
+
+      expect(() => renderer.create(subject)).toThrow(
+        'Unsupported number of cities'
+      );
+    });
+  });
+
+  describe('when there are six cities', () => {
+    it('renders an svg for the cities', () => {
+      const subject: React.ReactElement<City> = (
+        <City {...defaultProps} num={6} />
+      );
+
+      expect(renderer.create(subject)).toMatchSnapshot();
+    });
+  });
 });
